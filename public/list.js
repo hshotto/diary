c7c1e96b8547eb27c116d4137c717b716dc30355
shhwang <shhwang@future.co.kr>
2026-02-19 07:15:10 +0000

    검색 기능 추가

--- public/list.js
+++ public/list.js
@@ -1,6 +1,5 @@
 const boardStr = localStorage.getItem("board");
 const allDeleteBtn = document.querySelector("#allDelete");
-
 if (boardStr === null) {
     const listSrt = JSON.stringify([]);
     localStorage.setItem("board", listSrt);
@@ -26,6 +25,36 @@ const tbody = document.querySelector("tbody")
 
 for (let i = 0; i < boardObj.length; i++) {
     tbody.innerHTML += template(i, boardObj[i]);
+}
+
+const items = Array.from(document.querySelectorAll("tbody tr td:nth-child(2)")).map(item => item.innerText);
+const searchInput = document.getElementById('searchInput');
+const list = document.querySelector("#searchResult");
+
+searchInput.addEventListener('keyup', (e) => {
+    const keyword = e.target.value.toLowerCase();
+
+    if (keyword === "") {
+        const rows = document.querySelectorAll("tbody tr");
+        rows.forEach(tr => {
+            tr.style.display = "";
+        });
+        return;
+    }
+    const rows = document.querySelectorAll("tbody tr");
+    rows.forEach(tr => {
+        const titleText = tr.querySelector("td:nth-child(2)").innerText.toLowerCase();
+        if (titleText.includes(keyword)) {
+            tr.style.display = "";
+        }
+        else {
+            tr.style.display = "none";
+        }
+    })
+});
+
+function renderList(data) {
+    list.innerHTML = data.map(item => `<li>${item}</li>`).join('');
 }
 
 const allDeleteBtnHandler = (e) => {
