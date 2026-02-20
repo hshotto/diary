4027f1c04a952a9b50a1df01881e632572c53e1c
shhwang <shhwang@future.co.kr>
2026-02-20 04:41:54 +0000

    날짜 필터링 추가

--- public/list.js
+++ public/list.js
@@ -8,26 +8,28 @@ if (boardStr === null) {
 
 const boardObj = JSON.parse(boardStr);
 
-const template = (index, objValue) => {
-    return `
-    <tr>
-        <td>${index + 1}</td>
-        <td><a href="/board/view.html?index=${objValue.index}">${objValue.subject}</a></td>
-        <td>${objValue.writer}</td>
-        <td>${objValue.date}</td>
-        <td>${objValue.time}</td>
-        <td>${objValue.views}</td>
-        <td>${objValue.mood === undefined ? "-" : objValue.mood}</td>
-    </tr>
-    `;
-};
+//날짜 필터링
+function filterByDate(start, end) {
+    const startDate = start.format('YYYY-MM-DD');
+    const endDate = end.format('YYYY-MM-DD');
 
-const tbody = document.querySelector("tbody")
+    const tbody = document.querySelector("table tbody");
+    const items = Array.from(tbody.querySelectorAll("tr"));
 
-for (let i = 0; i < boardObj.length; i++) {
-    tbody.innerHTML += template(i, boardObj[i]);
+    items.forEach(item => {
+        const itemDateStr = item.querySelector("td:nth-child(4)").innerText.trim();
+        const itemDate = moment(itemDateStr).format('YYYY-MM-DD');
+
+        if (itemDate >= startDate && itemDate <= endDate) {
+            item.style.display = "";
+        }
+        else {
+            item.style.display = "none";
+        }
+    });
 }
 
+//제목 필터링
 const items = Array.from(document.querySelectorAll("tbody tr td:nth-child(2)")).map(item => item.innerText);
 const searchInput = document.getElementById('searchInput');
 
@@ -52,6 +54,26 @@ searchInput.addEventListener('keyup', (e) => {
         }
     })
 });
+
+const template = (index, objValue) => {
+    return `
+    <tr>
+        <td>${index + 1}</td>
+        <td><a href="/board/view.html?index=${objValue.index}">${objValue.subject}</a></td>
+        <td>${objValue.writer}</td>
+        <td>${objValue.date}</td>
+        <td>${objValue.time}</td>
+        <td>${objValue.views}</td>
+        <td>${objValue.mood === undefined ? "-" : objValue.mood}</td>
+    </tr>
+    `;
+};
+
+const tbody = document.querySelector("tbody")
+
+for (let i = 0; i < boardObj.length; i++) {
+    tbody.innerHTML += template(i, boardObj[i]);
+}
 
 function renderList(data) {
     list.innerHTML = data.map(item => `<li>${item}</li>`).join('');
